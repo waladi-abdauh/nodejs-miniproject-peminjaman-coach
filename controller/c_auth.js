a46c7bc7ca7b77a5633abee85fb6b954de58b110
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-03-13 21:56:00 +0700

    auth: proses cek username & password encrypt

--- controller/c_auth.js
+++ controller/c_auth.js
@@ -1,9 +1,34 @@
+const bcrypt = require('bcryptjs')
+const m_user = require('../model/m_user')
+
 module.exports = 
 {
 
     form_login:
     function (req,res) {
         res.render('auth/form-login')
+    },
+
+
+
+    proses_login:
+    async function (req,res) {
+        let username = req.body.form_username
+        let password = req.body.form_password
+        let get_user = await m_user.get_one(username)
+
+        // cek username
+        if (get_user.length > 0) {
+            // cek password
+            let password_match = bcrypt.compareSync(password, get_user[0].password)
+            if (password_match) {
+                res.send('password betul')
+            } else {
+                res.send('password salah')
+            }
+        } else {
+            res.send('user belum terdaftar')
+        }
     },
 
 
