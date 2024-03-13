17f47fed86e6832535df6ebb318b645dfc35e96f
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-03-13 20:04:09 +0700

    setting app

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('mini project peminjaman')
+const express   = require('express')
+const app       = express()
+const port      = 3000
+
+
+app.use(express.urlencoded({extended: false}))
+
+
+app.set('view engine', 'ejs')
+app.set('views', './view')
+
+
+app.listen(port, ()=>{
+    console.log('Aplikasi anda sudah siap, silakan buka http://localhost:' + port)
+})
