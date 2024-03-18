fa524b9a36fd282809eae556090985e433bef518
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-03-18 20:30:37 +0700

    rapihin script & uninstall passport

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-const express   = require('express')
-const app       = express()
-const port      = 3000
-const c_home    = require('./controller/c_home')
-const c_auth    = require('./controller/c_auth')
-const session           = require('express-session')
-const connectFlash      = require('connect-flash')
-const cookieParser      = require('cookie-parser')
-const passport          = require('passport')
+const express       = require('express')
+const app           = express()
+const port          = 3000
+const session       = require('express-session')
+const connectFlash  = require('connect-flash')
+const cookieParser  = require('cookie-parser')
+
+const c_home        = require('./controller/c_home')
+const c_auth        = require('./controller/c_auth')
 
 
+app.use(connectFlash())
 app.use(cookieParser('secret'))
 app.use(session({
-    secret:'secret', resave:true, saveUninitialized:false, cookie:{
-        maxAge: 1000 * 60 * 60 * 24 // 86400000 1 day
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 2
+        // 1000 milidetik * 60  = 1 menit
+        // 1 menit * 60         = 1 jam
+        // 1 jam * durasi       = x jam (aplikasi expired dalam x jam)
     }
 }))
-app.use(connectFlash())
-app.use(passport.initialize())
-app.use(passport.session())
 
 
 app.use(express.urlencoded({extended: false}))
@@ -30,32 +34,7 @@ app.set('views', './view')
 
 app.get('/', c_home.index)
 app.get('/auth', c_auth.form_login)
-
-
-
-cek_logout = (req,res,next)=>{
-    if (req.isAuthenticated()) {
-        return res.redirect('/');
-    }
-    next();
-}
-
-let otentikasiGagal =
-passport.authenticate('local', {
-    failureRedirect : '/auth/login',
-    failureFlash    : true
-})
-
-
-let otentikasiSukses =
-(req,res)=>{
-    if (req.session) {
-        res.redirect('/home/produksi/data/mesin-produksi')
-    }
-}
-
 app.post('/auth/proses-login', c_auth.proses_login)
-
 app.get('/dashboard', function(req,res) {
     if (req.session.user) {
         res.send('login & session sukses' + JSON.stringify(req.session))
