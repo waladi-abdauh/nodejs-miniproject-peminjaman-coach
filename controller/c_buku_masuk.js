87b3c2f58c82edb2897d6d46afbe6ffa94713c41
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2024-03-26 19:56:42 +0700

    buku masuk: proses insert

--- controller/c_buku_masuk.js
+++ controller/c_buku_masuk.js
@@ -1,14 +1,22 @@
-const moment = require('moment')
+const moment        = require('moment')
+const m_jenis_buku  = require('../model/m_jenis_buku')
+const m_genre       = require('../model/m_genre')
+const m_stok_buku   = require('../model/m_stok_buku')
+
 moment.locale('id')
+
+
 
 module.exports =
 {
 
 
     index:
-    function(req,res) {
+    async function(req,res) {
         res.render('template/layout', {
             konten          : 'buku-masuk/index',
+            jenis_buku      : await m_jenis_buku.get_all(),
+            genre           : await m_genre.get_all(),
             uri_segment     : req.path.split('/'),
             flash_message   : req.flash(),
         })
@@ -25,9 +33,10 @@ module.exports =
             genre           : req.body.form_genre,
             pengarang       : req.body.form_pengarang,
             ilustrator      : req.body.form_ilustrator,
+            kategori        : req.body.form_kategori,
             jumlah_masuk    : req.body.form_jumlah_masuk,
             jumlah_keluar   : 0,
-            jumlah_sisa     : 0,
+            jumlah_sisa     : req.body.form_jumlah_masuk,
             created_at      : moment().format('YYYY-MM-DD HH:mm:ss'),
             created_by      : req.session.user.id,
         }
@@ -38,10 +47,9 @@ module.exports =
                 res.redirect('/buku-masuk')
             }
         } catch (error) {
-            req.flash('danger', JSON.stringify(error))
-            res.redirect('/buku-masuk')
+            throw error
         }
-    }
+    },
 
 
 
--- model/m_stok_buku.js
+++ model/m_stok_buku.js
@@ -12,9 +12,9 @@ module.exports =
         try {
             let sql = await db.query(
                 `INSERT INTO ${table}
-                (nama, penerbit, jenis, genre, pengarang, ilustrator, jumlah_masuk, jumlah_keluar, jumlah_sisa, created_at, created_by)
-                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);`,
-                {bind: [data.nama, data.penerbit, data.jenis, data.genre, data.pengarang, data.ilustrator, data.jumlah_masuk, data.jumlah_keluar, data.jumlah_sisa, data.created_at, data.created_by]}
+                (nama, penerbit, jenis, genre, pengarang, ilustrator, kategori, jumlah_masuk, jumlah_keluar, jumlah_sisa, created_at, created_by)
+                VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);`,
+                {bind: [data.nama, data.penerbit, data.jenis, data.genre, data.pengarang, data.ilustrator, data.kategori, data.jumlah_masuk, data.jumlah_keluar, data.jumlah_sisa, data.created_at, data.created_by]}
             )
             return sql[0]
         } catch (error) {
